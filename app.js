b70b219839686da5a3c9f74662e6755c8280069d
Rahul <rahulgrover4841@gmail.com>
2022-12-04 17:46:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById("btn")
+let btnIcon= document.getElementById("btnIcon")
+let btnText= document.getElementById("btnText")
+btn.onclick=()=>{
+    document.body.classList.toggle("dark-theme")
+    if(document.body.classList.contains("dark-theme")){
+        btnIcon.src= "./sun.png"
+        btnText.textContent="Light"
+    }
+    else{
+        btnIcon.src= "./moon.png"
+        btnText.textContent="Dark"
+    }
+}
